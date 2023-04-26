2a02b8d6764940425addf8eac247571b4a595bac
Sam Wright <samwright@Sams-MacBook-Pro-3.local>
2023-04-26 00:31:23 -0500

    initial files

---
+++ Develop/client/src/js/database.js
@@ -0,0 +1,28 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  await openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('a jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('a jate database has been created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+
+
+};
+
+console.error('putDb not implemented');
+
+
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => console.error('getDb not implemented');
+
+initdb();
